9aed98f8cbe48ebeccd35efc90d92265f2c202c7
kenye <kenye@gmail.com>
2021-12-06 07:25:39 -0500

    primer commit

---
+++ seccion 3/app.js
@@ -0,0 +1,5 @@
+
+let c;
+let f=90;
+c=(f-32)*(5/9);
+console.log(c)
---
+++ seccion 4/app.js
@@ -0,0 +1,8 @@
+
+/*let nombre='ken';
+let apellido='Delgado';
+let nombreCompleto=nombre+' '+apellido;
+console.log(nombreCompleto);*/
+
+
+
